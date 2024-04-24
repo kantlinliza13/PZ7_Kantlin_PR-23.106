169df1d45d3bbeab1e1fe883c029e4a6dbe0fa3c
Elizaveta <Elizaveta.Kantlin2021@yandex.ru>
2024-04-24 17:02:51 +0700

    создали проект

---
+++ app/src/main/java/com/example/pz7_kantlin_pr_23106/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pz7_kantlin_pr_23106;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PZ7_Kantlin_PR-23.106"
+include(":app")
+ 
